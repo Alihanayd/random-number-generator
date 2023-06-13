6e5a16d2e595cd2822c4ace2020eaf3d8cce839d
Szqii <szqii.altn@gmail.com>
2023-06-13 22:33:54 +0300

    add():: custom input component

---
+++ src/components/HomeViewPartials/CustomInput.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="input-container">
+    <label for="input">{{ label }}</label>
+    <input
+        id="input"
+        :value="value"
+        @input="sendEmit($event.target.value)"
+        type="number"
+        placeholder="Min Limit"
+    />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "CustomInput",
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    sendEmit(value) {
+      this.$emit("input", parseInt(value));
+    }
+  }
+}
+
+</script>
+
+<style scoped lang="scss">
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+}
+
+.input-container label {
+  color: #fff;
+  margin-bottom: 12px;
+}
+
+.input-container input {
+  width: 120px;
+  height: 50px;
+  margin: 0 10px;
+  border-radius: 5px;
+  border: none;
+  text-align: center;
+  font-size: 24px;
+  color: #2c363f;
+}
+</style>
--- src/components/HomeViewPartials/Main.vue
+++ src/components/HomeViewPartials/Main.vue
@@ -2,57 +2,62 @@
   <div>
     <h1 class="number">{{ randomNumber }}</h1>
     <div class="button-container">
-      <button @click="randomNumberMaker" class="generate-button" type="button">
+      <button @click="setRandomNumber" class="generate-button" type="button">
         Generate Number
       </button>
     </div>
-    <div class="limitInp">
-      <input
-        :value="minValue"
-        @input="sendMinValue($event.target.value)"
-        type="number"
-        placeholder="Min Limit"
+    <div class="limit-inputs-container">
+
+      <custom-input
+          :label="'Min Limit'"
+          :value="min"
+          @input="(value) => sendMinValue(value)"
       />
-      <input
-        :value="maxValue"
-        @input="sendMaxValue($event.target.value)"
-        type="number"
-        placeholder="Max Limit"
-      />
+
+      <custom-input
+          :label="'Max Limit'"
+          :value="max"
+          @input="(value) => sendMaxValue(value)"/>
     </div>
   </div>
 </template>
 
 <script>
+import CustomInput from "@/components/HomeViewPartials/CustomInput.vue";
+
 export default {
   name: "Main",
+  components: {CustomInput},
   props: {
-    msg: String,
-    randomNumber: Number,
-    minValue: Number,
-    maxValue: Number,
+    randomNumber: {
+      type: Number,
+      required: true,
+    },
+    min: {
+      type: Number,
+      required: true,
+    },
+    max: {
+      type: Number,
+      required: true,
+    }
   },
   methods: {
-    randomNumberMaker() {
-      this.$emit("randomNumberMaker");
+    setRandomNumber() {
+      this.$emit("setRandomNumber");
     },
     sendMinValue(value) {
-      console.log(value, typeof value);
-      this.$emit("minValue", parseInt(value));
+      this.$emit("setMin", parseInt(value));
     },
     sendMaxValue(value) {
-      this.$emit("maxValue", parseInt(value));
+      this.$emit("setMax", parseInt(value));
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
+
 .generate-button {
   background-color: #6cd4ff;
   border: none;
@@ -64,29 +69,22 @@ input::-webkit-inner-spin-button {
   margin: 4px 2px;
   cursor: pointer;
 }
+
 .button-container {
   display: flex;
   justify-content: center;
 }
+
 .number {
   font-size: 3rem;
   text-align: center;
   margin: 2rem 0;
   color: #fff;
 }
-.limitInp {
+
+.limit-inputs-container {
   display: flex;
   justify-content: center;
   padding: 2rem 0;
-}
-.limitInp input {
-  width: 100px;
-  height: 30px;
-  margin: 0 10px;
-  border-radius: 5px;
-  border: none;
-  text-align: center;
-  font-size: 16px;
-  color: #2c363f;
 }
 </style>
