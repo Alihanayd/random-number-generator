0b0d986a04e063f3b3a5f1ae2af675a02fc1384e
alihanayd <aaydin184@posta.pau.edu.tr>
2023-06-13 23:34:53 +0300

    add():: error message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <Title/>
+    <Title />
     <Main
-        :randomNumber="parseInt(randomNumber)"
-        @setRandomNumber="() => setRandomNumber()"
-        :min="minValue"
-        :max="maxValue"
-        @setMin="minValue = $event"
-        @setMax="maxValue = $event"
+      :randomNumber="parseInt(randomNumber)"
+      @setRandomNumber="() => setRandomNumber()"
+      :min="minValue"
+      :max="maxValue"
+      @setMin="(value) => setMin(value)"
+      @setMax="(value) => setMax(value)"
     />
+    <div v-if="isError" class="alert">
+      {{ errorMessage }}
+    </div>
   </div>
 </template>
 
@@ -27,15 +30,46 @@ export default {
       randomNumber: 0,
       minValue: 0,
       maxValue: 100,
+      isError: false,
+      errorMessage: "",
     };
   },
   methods: {
     setRandomNumber() {
-      this.randomNumber = Math.floor(Math.random() * (this.maxValue - this.minValue + 1)) + this.minValue;
-      console.log('Random Number: ', this.randomNumber);
-      console.log('Min Value: ', this.minValue);
-      console.log('Max Value: ', this.maxValue);
+      if (this.isError) {
+        return;
+      }
+      this.randomNumber =
+        Math.floor(Math.random() * (this.maxValue - this.minValue + 1)) +
+        this.minValue;
+    },
+    isMinValueGreaterThanMaxValue() {
+      this.isError = this.minValue > this.maxValue;
+      if (this.isError) {
+        this.errorMessage = "Min value cannot be greater than max value";
+      }
+    },
+    setMin(value) {
+      this.minValue = value;
+      this.isMinValueGreaterThanMaxValue();
+    },
+    setMax(value) {
+      this.maxValue = value;
+      this.isMinValueGreaterThanMaxValue();
     },
   },
 };
 </script>
+<style lang="scss" scoped>
+.alert {
+  padding: 20px;
+  background-color: #d13126;
+  color: white;
+  width: 30%;
+  margin: 0 auto;
+  text-align: center;
+  font-size: 24px;
+  font-weight: 700;
+  border-radius: 5px;
+}
+</style>
